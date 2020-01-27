f2a90f0d729e9827deb3c2301524a86e1d6da20e
Sebastian Schulz <ss@gis-support.pl>
2020-01-27 16:36:50 +0100

    prod ver 3

--- docker/api/prod_start.sh
+++ docker/api/prod_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /api
-pipenv --three --site-packages && pipenv install --skip-lock && pipenv run uwsgi --ini uwsgi.ini
+pipenv --three --site-packages && pipenv install --skip-lock && pipenv run python uwsgi.py
+#TODO UWSGI: pipenv run uwsgi --ini uwsgi.ini
