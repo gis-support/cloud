b544e6e9741914bb4531d73b1d09c2b2312357dd
Sebastian Schulz <mail@schulzsebastian.com>
2020-02-18 15:15:02 +0100

    fix nginx env variables

--- docker/nginx/prod_start.sh
+++ docker/nginx/prod_start.sh
@@ -1,1 +1,7 @@
-curl https://${APP_PROD_HOST_URL}/api/docs/api.json -o /cloud/src/docs/api.json && cd /cloud && npm i && npm run build && echo 'Cloud application ready to use' && nginx -g 'daemon off;'
+curl https://${APP_PROD_HOST_URL}/api/docs/api.json -o /cloud/src/docs/api.json && \
+cd /cloud && \
+npm i && \
+npm run build && \
+envsubst '$$CONTAINER_BASENAME $$APP_PROD_API_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf && \
+echo 'Cloud application ready to use' && \
+nginx -g 'daemon off;'
