e76aa4948da782aeff5946411939f48302235e80
Kamil Kozik <kamil.kozik@gis-support.pl>
2020-06-25 13:48:35 +0200

    CGS-31: skrypt uruchamiający migracje;

---
+++ api/app/db/migrations/migration.py
@@ -0,0 +1,18 @@
+from peewee import AutoField, TextField
+
+from app.db.base_model import BaseModel
+
+
+class Migration(BaseModel):
+    """Model przechowujący nazwy dotychczas wykonanych skryptów migracyjnych"""
+
+    class Meta:
+        table_name = "versions"
+        schema = "system"
+
+    id = AutoField(primary_key=True)
+    script = TextField()
+
+    @staticmethod
+    def script_sorter(name):
+        return int(name.split('_')[0])
