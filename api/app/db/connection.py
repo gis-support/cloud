02b0f57b34fb719f046926a5b1edf0d6dff39348
Kamil Kozik <kamil.kozik@gis-support.pl>
2020-07-13 15:03:00 +0200

    CGS-51

--- api/app/db/connection.py
+++ api/app/db/connection.py
@@ -1,4 +1,11 @@
+from peewee import PostgresqlDatabase
+
 from app.db.database import database
+
+
+def init_priviliges(database: PostgresqlDatabase):
+    with database.atomic():
+        database.execute_sql("GRANT SELECT on layer_styles to public;")
 
 
 def create_db(config):
@@ -7,4 +14,7 @@ def create_db(config):
                   user=config['DBUSER'],
                   password=config['DBPASS'],
                   port=config['DBPORT'])
+
+    init_priviliges(database)
+
     return database
