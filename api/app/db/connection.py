ceccb3ae5d1748b5e0e16fe49b2ef7f524647a46
Kamil Kozik <kamil.kozik@gis-support.pl>
2020-07-13 19:59:09 +0200

    Revert "CGS-51" 02b0f57b

--- api/app/db/connection.py
+++ api/app/db/connection.py
@@ -1,11 +1,4 @@
-from peewee import PostgresqlDatabase
-
 from app.db.database import database
-
-
-def init_priviliges(database: PostgresqlDatabase):
-    with database.atomic():
-        database.execute_sql("GRANT SELECT on layer_styles to public;")
 
 
 def create_db(config):
@@ -14,7 +7,4 @@ def create_db(config):
                   user=config['DBUSER'],
                   password=config['DBPASS'],
                   port=config['DBPORT'])
-
-    init_priviliges(database)
-
     return database
