0aca81fe1ea6edc5ff5d8d5d45d0c88a15569e3d
js-gissupport <jakub.skowronski@gis-support.pl>
2020-09-10 13:27:06 +0200

    SD-800 Zmiana sposobu usuwania warstw dodatkowych w projektach

--- api/app/blueprints/layers/routings.py
+++ api/app/blueprints/layers/routings.py
@@ -202,7 +202,7 @@ def layers_id(lid):
         """
         @layer_decorator(permission="owner")
         def delete(layer, lid=None):
-            Project.update_additional_layers_ids(layer.lid, '')
+            Project.delete_additional_layer_id(layer.lid)
             Project.delete().where(Project.active_layer_id == layer.lid).execute()
             layer.delete()
             return jsonify({"layers": "{} deleted".format(layer.name)})
